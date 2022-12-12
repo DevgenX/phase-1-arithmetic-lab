95349013ffac3a80e66ee74715d22202c3683f09
devgenx <sebastian110420@gmail.com>
2022-12-11 19:39:02 -0500

    commit tree 9e9504331ef6c4c0eb0226f79cfd11097fe730ae

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
-
 // Write your code here
+
+let num1 = 31;
+let num2 = 2;
+
+let num3 = 9;
+let num4 = 40;
+
+const multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const mod = num4 % num3;
+
+let max = Math.max(1, 2, 3, 4, 20);
